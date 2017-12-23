44feb1a9bb2fecc4fa48ed462b8eda49060b6fb9
Young Jun Park <june0313@gmail.com>
2017-12-23 15:42:43 +0900

    [baekjoon] 2750번 문제 풀이

---
+++ src/main/java/beakjoon/yj/p_2750/Main.java
@@ -0,0 +1,34 @@
+package beakjoon.yj.p_2750;
+
+import java.util.Scanner;
+
+/**
+ * https://www.acmicpc.net/problem/2750
+ *
+ * N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] > numbers[j]) {
+                    int temp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
+                }
+            }
+        }
+
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
