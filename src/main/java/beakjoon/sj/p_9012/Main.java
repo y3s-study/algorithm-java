659b39b00ab2c3478ba4676ac2ac8de9945d9ffd
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2017-12-27 19:33:09 +0900

    beakjoon [9012] 문제 풀이 작성
    stack 에서 removeAllElements와 clear는 같은 기능을 함.

---
+++ src/main/java/beakjoon/sj/p_9012/Main.java
@@ -0,0 +1,37 @@
+package beakjoon.sj.p_9012;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+//9012
+public class Main {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int i, j;
+		char[][] input = new char[n][];
+		Stack<Integer> stack = new Stack<>();	//0:(  1: ) 
+		
+		for(i=0; i<n; i++){
+			input[i] = sc.next().toCharArray();
+			for(j=0; j<input[i].length; j++){
+				if(input[i][j] == '('){
+					stack.add(0);
+				}else{
+					if(stack.empty()){
+						break;
+					}else{
+						stack.remove(stack.size()-1);
+					}
+				}
+			}
+			if(j == input[i].length && stack.empty()){
+				System.out.println("YES");
+			}else{
+				System.out.println("NO");
+			}
+//			stack.removeAllElements();
+			stack.clear();
+		}
+	}
+}
