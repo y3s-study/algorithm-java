991bb11c817c8f445ef940dbbbd7471940c5aff4
123231 <123231@123231-PC>
2018-01-02 01:38:41 +0900

    [baekjoon] 10828 문제 풀이 작성

---
+++ src/main/java/baekjoon/sb/p_10828/Main.java
@@ -0,0 +1,88 @@
+package baekjoon.sb.p_10828;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = 0;
+		String input = null;
+		String [] inputArray;
+		Stack stack = new Stack();
+		
+		n = sc.nextInt();
+		sc.nextLine();
+		
+		for(int i=0; i<n; i++) {
+			input = sc.nextLine();
+			inputArray = input.split(" ");
+			
+			switch(inputArray[0]) {
+				case "push":
+					stack.push(Integer.parseInt(inputArray[1]));
+					break;
+				case "pop":
+					System.out.println(stack.pop());
+					break;
+				case "size":
+					System.out.println(stack.getSize());
+					break;
+				case "empty":
+					System.out.println(stack.isEmpty());
+					break;
+				case "top":
+					System.out.println(stack.top());
+					break;
+			}
+			
+		}
+		
+		sc.close();
+
+	}
+
+}
+
+class Stack {
+	
+	private ArrayList<Integer> list = new ArrayList<>();
+	private int top = -1;
+	
+	public void push(int i) {
+		list.add(i);
+		top++;
+	}
+	
+	public int pop() {
+		int n = -1;
+		if(top >= 0) {
+			n = list.get(top);
+			list.remove(top--);
+		}
+		return n;
+	}
+	
+	public int getSize() {
+		return top+1;
+	}
+	
+	public int isEmpty() {
+		if(top == -1) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	
+	public int top() {
+		if(top >= 0) {
+			return list.get(top);
+		}
+		else {
+			return -1;
+		}
+	}
+}
