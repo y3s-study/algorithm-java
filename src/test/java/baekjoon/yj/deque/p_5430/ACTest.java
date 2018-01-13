6ce33218f80a6d641852d25e8715924c97ebe37b
Young Jun Park <june0313@gmail.com>
2018-01-13 11:08:12 +0900

    [baekjoon-5430] 최악의 경우에 대한 테스트 케이스 추가
    - 시간 측정을 위해 test 코드에서만 사용할 수 있도록 guava 라이브러리 추가

--- src/test/java/baekjoon/yj/deque/p_5430/ACTest.java
+++ src/test/java/baekjoon/yj/deque/p_5430/ACTest.java
@@ -1,7 +1,13 @@
 package baekjoon.yj.deque.p_5430;
 
 
+import com.google.common.base.Stopwatch;
 import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -35,5 +41,25 @@ public class ACTest {
     public void testCase5() {
         String result = AC.of("DRD", "[10, 20]").compute();
         assertThat(result).isEqualTo("[]");
+    }
+
+    /**
+     * 최악의 경우 1000ms 안에 계산이 완료 되어야 한다.
+     */
+    @Test(timeout = 1000)
+    public void testWorstCase() {
+        // given
+        String functionStr = IntStream.rangeClosed(1, 100_000).mapToObj(i -> i <= 99_900 ? "R" : "D").collect(Collectors.joining());
+        String elements = Stream.generate(() -> "1").limit(100).collect(Collectors.joining(",", "[", "]"));
+
+        // when
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        String result = AC.of(functionStr, elements).compute();
+        stopwatch.stop();
+
+        // then
+        assertThat(result).isEqualTo("[]");
+
+        System.out.println("worst case elapsed time : " + stopwatch.elapsed(TimeUnit.MILLISECONDS) + " ms");
     }
 }
