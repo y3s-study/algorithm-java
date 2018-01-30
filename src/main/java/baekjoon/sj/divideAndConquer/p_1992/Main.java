2b7903ce0d9ba65d501ae5306da1e24c4eda97ae
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-01-30 13:54:07 +0900

    [baekjoon] 1992 쿼드 트리
    중복실행되는 코드 삭제
    (for문 제거)

--- src/main/java/baekjoon/sj/divideAndConquer/p_1992/Main.java
+++ src/main/java/baekjoon/sj/divideAndConquer/p_1992/Main.java
@@ -4,13 +4,10 @@ import java.util.Scanner;
 
 //1992
 public class Main {
-	static boolean visit[][];
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		char[][] arr = new char[n][n];
-		visit = new boolean[n][n];
 
 		for (int i = 0; i < n; i++) {
 			arr[i] = sc.next().toCharArray();
@@ -20,36 +17,20 @@ public class Main {
 	}
 
 	static String quadTree(char[][] arr, int startRow, int startCol, int size) {
+		if (size == 0) {
+			return String.valueOf(arr[startRow][startCol]);
+		}
 		String result = "";
 		int pivot = (size + 1) / 2;
-		if (size == 0) {
-			visit[startRow][startCol] = true;
-			return result = "" + arr[startRow][startCol];
-		}
-		for (int row = startRow; row <= startRow + size; row++) {
-			for (int col = startCol; col <= startCol + size; col++) {
-				if (visit[row][col] == false) {
-					// left - top
-					if (row < startRow + pivot && col < startCol + pivot) {
-						result += "(" + quadTree(arr, startRow, startCol, pivot - 1);
-					}
-					// right - top
-					else if (row < startRow + pivot && col >= startCol + pivot) {
-						result += quadTree(arr, startRow, startCol + pivot, pivot - 1);
-					}
-					// left - bottom
-					else if (row >= startRow + pivot && col < startCol + pivot) {
-						result += quadTree(arr, startRow + pivot, startCol, pivot - 1);
-					}
-					// right - bottom
-					else if (row >= startRow + pivot && col >= startCol + pivot) {
-						result += quadTree(arr, startRow + pivot, startCol + pivot, pivot - 1) + ")";
-						result = result.replaceAll("\\(0+\\)", "0").replaceAll("\\(1+\\)", "1");
-					}
-				}
-			}
-		}
+		// left - top
+		result += "(" + quadTree(arr, startRow, startCol, pivot - 1);
+		// right - top
+		result += quadTree(arr, startRow, startCol + pivot, pivot - 1);
+		// left - bottom
+		result += quadTree(arr, startRow + pivot, startCol, pivot - 1);
+		// right - bottom
+		result += quadTree(arr, startRow + pivot, startCol + pivot, pivot - 1) + ")";
 
-		return result;
+		return result.replaceAll("\\(0+\\)", "0").replaceAll("\\(1+\\)", "1");
 	}
 }
