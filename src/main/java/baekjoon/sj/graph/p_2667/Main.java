f0e268a24ca3d7640cbb652d8b9db72462aeadcd
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-03-04 20:18:36 +0900

    [baekjoon] 단지번호붙이기
    static 전역 변수(visit)를 로컬 변수로 변경

--- src/main/java/baekjoon/sj/graph/p_2667/Main.java
+++ src/main/java/baekjoon/sj/graph/p_2667/Main.java
@@ -6,13 +6,11 @@ import java.util.Scanner;
 
 //2667
 public class Main {
-	static boolean[][] visit;
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		char[][] arr = new char[n][n];
-		visit = new boolean[n][n];
+		boolean[][] visit = new boolean[n][n];
 
 		for (int i = 0; i < n; i++) {
 			arr[i] = sc.next().toCharArray();
