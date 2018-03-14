d700c84c51ea038e7ccbd906488c09f7600d6c32
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-03-14 23:22:10 +0900

    [baekjoon] 11003 최소값 찾기 - 시간초과(53%)
    deque 를 이용한 슬라이딩 윈도우 알고리즘 구현 53퍼 시간초과

--- src/main/java/baekjoon/sj/slidingWindow/p_11003/Main.java
+++ src/main/java/baekjoon/sj/slidingWindow/p_11003/Main.java
@@ -1,26 +1,48 @@
 package baekjoon.sj.slidingWindow.p_11003;
 
-import java.util.PriorityQueue;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 //11003
 public class Main {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int L = sc.nextInt();
 		int[] arr = new int[N];
 
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		Deque<Pair> dq = new LinkedList<>();
 		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<arr.length; i++){
+		for (int i = 0; i < arr.length; i++) {
 			arr[i] = sc.nextInt();
-			if(i >= L){
-				pq.remove(arr[i-L]);
+			while (!dq.isEmpty() && dq.getFirst().getIndex() < i - L + 1) {
+				dq.removeFirst();
 			}
-			pq.add(arr[i]);
-			sb.append(pq.peek()+"\n");
+			while (!dq.isEmpty() && dq.getLast().getValue() >= arr[i]) {
+				dq.removeLast();
+			}
+			dq.add(new Pair(i, arr[i]));
+			sb.append(dq.getFirst().getValue() + " ");
 		}
-		System.out.println(sb.toString());
+		System.out.println(sb);
+	}
+}
+
+class Pair {
+	private int index;
+	private int value;
+
+	Pair(int index, int value) {
+		this.index = index;
+		this.value = value;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public int getValue() {
+		return value;
 	}
 }
