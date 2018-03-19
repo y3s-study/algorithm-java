9dace9cc1e5521f65e05b57478ac92721f4b2000
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-03-19 20:55:48 +0900

    [baekjoon] 11003 최소값 - 문제 풀이 완료
    입력 형식 변경
    (scanner에서 Bufferedreader로 변경)

--- src/main/java/baekjoon/sj/slidingWindow/p_11003/Main.java
+++ src/main/java/baekjoon/sj/slidingWindow/p_11003/Main.java
@@ -1,21 +1,29 @@
 package baekjoon.sj.slidingWindow.p_11003;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 //11003
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int L = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
 		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
 
 		Deque<Pair> dq = new LinkedList<>();
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < arr.length; i++) {
-			arr[i] = sc.nextInt();
 			while (!dq.isEmpty() && dq.getFirst().getIndex() < i - L + 1) {
 				dq.removeFirst();
 			}
