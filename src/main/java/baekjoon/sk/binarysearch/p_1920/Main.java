6fd19c1b7dbb7f5138f262dd0efbfe6ac9bc96f1
jsk00123 <jsk00123@naver.com>
2018-03-29 23:18:43 +0900

    [baekjoon] 수찾기 문제풀이 완료

--- src/main/java/baekjoon/sk/binarysearch/p_1920/Main.java
+++ src/main/java/baekjoon/sk/binarysearch/p_1920/Main.java
@@ -11,52 +11,33 @@ public class Main {
 		int N = sc.nextInt();
 
 		int A[] = new int[N];
-		
+
 		for (int i = 0; i < N; i++) {
 			A[i] = sc.nextInt();
 		}
 
 		Arrays.sort(A);
-		
+
 		int M = sc.nextInt();
 
 		for (int i = 0; i < M; i++) {
-			System.out.println(setHasNum(sc.nextInt(), A));
+			System.out.println(binarySearch(sc.nextInt(), A, 0, A.length - 1));
 		}
 	}
 
-	public static int setHasNum(int a, int[] A) {
+	public static int binarySearch(int a, int[] A, int start, int end) {
 
-		// A라는 배열 안에 존재하는지 확인
-		int result = binarySearch(a, A);
+		int mid = (start + end) / 2;
 
-		return result;
-	}
+		if (start > end)
+			return 0;
 
-	public static int binarySearch(int a, int[] A) {
-
-		if (A.length < 2) {
-			if (A[0] == a)
-				return 1;
-			else
-				return 0;
-		}
-
-		if (a < A[A.length / 2]) {
-			int[] B = new int[A.length / 2];
-
-			System.arraycopy(A, 0, B, 0, A.length / 2);
-
-			return binarySearch(a, B);
-		} else if (a == A[A.length / 2]) {
+		if (a < A[mid]) {
+			return binarySearch(a, A, start, mid - 1);
+		} else if (a == A[mid]) {
 			return 1;
 		} else {
-			int[] C = new int[A.length - A.length / 2];
-
-			System.arraycopy(A, A.length / 2 + 1, C, 0, A.length - A.length / 2
-					- 1);
-
-			return binarySearch(a, C);
+			return binarySearch(a, A, mid + 1, end);
 		}
 
 	}
