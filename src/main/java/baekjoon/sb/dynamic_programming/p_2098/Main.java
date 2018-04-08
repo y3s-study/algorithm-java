cb8ea8ac28fee502fa5c79b86ab0b4015ece4372
Song <ppsosqq@gmail.com>
2018-04-08 22:20:17 +0900

    [baekjoon-2098] 외판원 순회 문제 풀이

--- src/main/java/baekjoon/sb/dynamic_programming/p_2098/Main.java
+++ src/main/java/baekjoon/sb/dynamic_programming/p_2098/Main.java
@@ -4,12 +4,17 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
+	
+	public static int[][] W;
+	public static int[][] shortestPath;
+	public static int N;
+	public static final int INF = 1000000000;
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt(); // 도시의 수
+		N = sc.nextInt(); // 도시의 수
 		
-		int[][] W = new int[N+1][N+1]; // W[i][j] - i에서 j로 가기 위한 비용
+		W = new int[N+1][N+1]; // W[i][j] - i에서 j로 가기 위한 비용
 		
 		for(int i = 1; i <= N; i++) {
 			for(int j = 1; j <= N; j++) {
@@ -17,15 +22,15 @@ public class Main {
 			}
 		}
 		
-		int[][] shortestPath = new int[N+1][1 << N]; // shortestPath[current][visited] - 현재 위치 current에서 방문했던 도시들이 visited일 때, 부분 경로 최소값
-		for(int i = 0; i <= N; i++) {
+		shortestPath = new int[N+1][1 << N]; // shortestPath[current][visited] - 현재 위치 current에서 방문했던 도시들이 visited일 때, 부분 경로 최소값
+		for(int i = 1; i <= N; i++) {
 			Arrays.fill(shortestPath[i], -1);
 		}
 		
-		System.out.println(getShortestPath(W, shortestPath, N, 1, 1));
+		System.out.println(getShortestPath(1, 1));
 	}
 	
-	public static int getShortestPath(int[][] W, int[][] shortestPath, int N, int current, int visited) {
+	public static int getShortestPath(int current, int visited) {
 		
 		if(visited == (1 << N) - 1) { // 모든 정점을 방문한 경우
 			return W[current][1];
@@ -35,7 +40,7 @@ public class Main {
 			return shortestPath[current][visited];
 		}
 		
-		int ret = Integer.MAX_VALUE;
+		int ret = INF;
 		
 		for(int i = 1; i <= N; i++) {
 			if((visited & (1 << (i - 1))) != 0) { // 이미 방문한 경로 jump
@@ -46,7 +51,7 @@ public class Main {
 				continue;
 			}
 			
-			int temp = W[current][i] + getShortestPath(W, shortestPath, N, i, visited + (1 << (i - 1)));
+			int temp = W[current][i] + getShortestPath(i, visited + (1 << (i - 1)));
 			ret = Math.min(ret, temp);
 		}
 		
