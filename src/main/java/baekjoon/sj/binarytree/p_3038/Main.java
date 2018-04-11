d9dcfa0608f24aa9021ede73b11b96a88c85bb9a
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-04-11 14:55:49 +0900

    [baekjoon] 3038 완전 이진 트리 - 문제 풀이 완료

--- src/main/java/baekjoon/sj/binarytree/p_3038/Main.java
+++ src/main/java/baekjoon/sj/binarytree/p_3038/Main.java
@@ -10,50 +10,65 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int[] tree = new int[(int) (Math.pow(2, n) + 1)];
+		int[] tree = new int[(int) (Math.pow(2, n + 1) + 1)];
 		for (int i = 0; i < n; i++) {
 			creatTree(tree, i);
-			int k = 1;
-			while (tree[k] != 0) {
-				System.out.print(tree[k] + " ");
-				k++;
-			}
-			System.out.println();
 		}
+		preOrder(tree, 1);
 	}
 
-	private static void creatTree(int[] tree, int n) {
-		if (n == 0) {
+	private static void preOrder(int[] tree, int index) {
+		if (tree[index] == 0) {
+			return;
+		}
+		System.out.print(tree[index] + " ");
+		preOrder(tree, index * 2);
+		preOrder(tree, index * 2 + 1);
+	}
+
+	private static void creatTree(int[] tree, int depth) {
+		if (depth == 0) {
 			tree[1] = 1;
 			return;
 		}
-		int[] copyTree = new int[(int) Math.pow(2, n) + 1];
+		int[] copyTree = new int[tree.length];
 		System.arraycopy(tree, 0, copyTree, 0, copyTree.length);
 
-		int k = 1;
-		for (int i = 2; i < (int) Math.pow(2, n + 1); i++) {
-			if ((int) Math.pow(2, k) == i) {
-				k++;
+		int tempDepth = 1;
+		int copyIndex = 0;
+		int startCopyIndex = (int) Math.pow(2, tempDepth - 1);
+		int resetCopyIndex = (int) Math.pow(2, tempDepth) / 2;
+		for (int i = 2; i < (int) Math.pow(2, depth + 1); i++) {
+			if ((int) Math.pow(2, tempDepth) == i) {
+				startCopyIndex = (int) Math.pow(2, tempDepth - 1);
+				resetCopyIndex = (int) Math.pow(2, tempDepth) / 2;
+				copyIndex = 0;
+				tempDepth++;
 			}
-			System.out.println("n: " + n + " k: " + k);
-			if ((int) Math.pow(2, k - 1) < i && i < (int) Math.pow(2, k)) {
-				System.out.print("right ");
-				tree[i] = copyTree[i / 2] * 2;
-				if (n != k - 1) {
-					System.out.print("enter ");
+			if (copyIndex >= resetCopyIndex) {
+				copyIndex = 0;
+			}
+			tree[i] = copyTree[startCopyIndex + copyIndex] * 2;
+			copyIndex++;
+		}
+
+		tempDepth = 1;
+		for (int i = 2; i < (int) Math.pow(2, depth + 1); i++) {
+			if ((int) Math.pow(2, tempDepth) == i) {
+				tempDepth++;
+			}
+			int startIndex = (int) Math.pow(2, tempDepth - 1) + ((int) Math.pow(2, tempDepth) - (int) Math.pow(2, tempDepth - 1)) / 2;
+			int endIndex = (int) Math.pow(2, tempDepth);
+
+			if (startIndex <= i && i < endIndex) {
+				if (depth != tempDepth - 1) {
 					tree[i]++;
 				}
 			} else {
-				System.out.print("left ");
-				tree[i] = copyTree[i / 2] * 2;
-				if (n == k - 1) {
-					System.out.print("enter ");
+				if (depth == tempDepth - 1) {
 					tree[i]++;
 				}
 			}
-			System.out.println("tree[" + i + "]: " + tree[i]);
-
 		}
-
 	}
 }
