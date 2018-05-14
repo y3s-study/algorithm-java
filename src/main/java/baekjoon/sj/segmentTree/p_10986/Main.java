063e12ff37ee1ebdf720e6183c006f09c99fa4ae
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-05-14 14:54:57 +0900

    [baekjoon] 10986 나머지 합 - 문제 풀이 완료

--- src/main/java/baekjoon/sj/segmentTree/p_10986/Main.java
+++ src/main/java/baekjoon/sj/segmentTree/p_10986/Main.java
@@ -13,56 +13,22 @@ public class Main {
 		int m = sc.nextInt();
 
 		int[] arr = new int[n + 1];
+		long[] sum = new long[arr.length];
+		long[] remain = new long[arr.length];
 		for (int i = 1; i < arr.length; i++) {
 			arr[i] = sc.nextInt();
+			sum[i] = (arr[i] + sum[i - 1]);
 		}
 
-		int size = findSize(arr);
-		int[] segTree = new int[(int) Math.pow(2, size)];
+		for (int i = 1; i < remain.length; i++) {
+			remain[(int) (sum[i] % m)]++;
+		}
 
-		initSegTree(segTree, arr, 1, 1, arr.length - 1);
-
-		int cnt = 0;
-		for (int i = 1; i < arr.length; i++) {
-			for (int j = i; j < arr.length; j++) {
-				if (findSum(segTree, 1, 1, arr.length - 1, i, j) % m == 0) {
-					cnt++;
-				}
-			}
+		remain[0]++; // why?
+		long cnt = 0;
+		for (int i = 0; i < arr.length; i++) {
+			cnt += (remain[i] * (remain[i] - 1)) / 2;
 		}
 		System.out.println(cnt);
-	}
-
-	private static int findSum(int[] segTree, int node, int start, int end, int left, int right) {
-		if (left > end || right < start) {
-			return 0;
-		}
-
-		if (left <= start && end <= right) {
-			return segTree[node];
-		}
-
-		int mid = (start + end) / 2;
-
-		return findSum(segTree, node * 2, start, mid, left, right) + findSum(segTree, node * 2 + 1, mid + 1, end, left, right);
-	}
-
-	private static int initSegTree(int[] segTree, int[] arr, int node, int start, int end) {
-		if (start == end) {
-			return segTree[node] = arr[start];
-		}
-		int mid = (start + end) / 2;
-
-		return segTree[node] = initSegTree(segTree, arr, node * 2, start, mid) + initSegTree(segTree, arr, node * 2 + 1, mid + 1, end);
-	}
-
-	private static int findSize(int[] arr) {
-		int length = arr.length;
-		int cnt = 0;
-		while (length > Math.pow(2, cnt)) {
-			cnt++;
-		}
-
-		return cnt + 1;
 	}
 }
